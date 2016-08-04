dec6ba8a636fa0804ee4ef384a16e0c8f4da0068
André R <andre.romcke@gmail.com>
2016-08-04 09:14:31 +0100

    [Platform.sh] Add Redis, mariadb 10.0 & readline php ext (#29)
    * [Platform.sh] Update to use Redis, mariadb 10.0 & readline php extension
    
    * fixup changes from feedback, port, mysql service name and other fixes

--- app/config/parameters_platform.php
+++ app/config/parameters_platform.php
@@ -1,10 +1,14 @@
 <?php
-$relationships = getenv("PLATFORM_RELATIONSHIPS");
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
+
+$relationships = getenv('PLATFORM_RELATIONSHIPS');
 if (!$relationships) {
   return;
 }
 
-$relationships = json_decode(base64_decode($relationships), TRUE);
+$relationships = json_decode(base64_decode($relationships), true);
 
 foreach ($relationships['database'] as $endpoint) {
   if (empty($endpoint['query']['is_master'])) {
@@ -18,6 +22,20 @@ foreach ($relationships['database'] as $endpoint) {
   $container->setParameter('database_user', $endpoint['username']);
   $container->setParameter('database_password', $endpoint['password']);
   $container->setParameter('database_path', '');
+}
+
+if (!empty($relationships['redis'][0])) {
+    // Setup custom redis cache pool to use for the install.
+    $container->setParameter('cache_pool', 'platformredis');
+    $host = $relationships['redis'][0]['host'];
+    $port =  $relationships['redis'][0]['port'];
+    file_put_contents(
+        __DIR__ . '/cache_pool/platformredis.yml'
+        "stash:{caches:{platformredis:{drivers: [Redis], Redis: {servers: [{server: '${host}', port: ${port}}]}}}}"
+    );
+
+    $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/cache_pool'));
+    $loader->load('platformredis.yml');
 }
 
 # Store session into /tmp.
