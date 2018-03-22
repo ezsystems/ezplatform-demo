a9123974088ed9b7a02fb8bb165808c315a55d48
Damian Zabawa <damian.zabawa@ez.no>
2018-03-22 09:48:13 +0100

    EZEE-1900: Installer fails if auto_increment_increment != 1 (#76)
    * EZEE-1900: Installer fails if auto_increment_increment != 1
    
    https://jira.ez.no/browse/EZEE-1900

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -5,8 +5,15 @@
  */
 namespace AppBundle;
 
+use AppBundle\DependencyInjection\Compiler\MigrationParameterPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new MigrationParameterPass());
+    }
 }
